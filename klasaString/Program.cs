0258546ed16ffc3474015c7b5fb27d6d6b9c419e
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-10-18 09:30:45 +0200

    Add files via upload

---
+++ klasaString/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "Danas radimo sa klasama String.";
+
+            Console.WriteLine(tekst);
+            
+            Console.WriteLine("Broj znakova je " + tekst.Length);
+            Console.WriteLine("");
+
+            Console.WriteLine("Velika slova: " + tekst.ToUpper());
+
+            Console.WriteLine("Mala slova: " + tekst.ToLower());
+
+            Console.WriteLine("\n -- Substring metoda --");
+            Console.WriteLine(tekst.Substring(0,5));
+
+            Console.WriteLine(tekst.Substring(13, tekst.Length - 13));
+
+            Console.WriteLine("\n -- Zamjena znakova --");
+            Console.WriteLine(tekst.Replace("Danas", "Sutra"));
+
+            Console.WriteLine("\n -- Razdvajanje --");
+            string[] rijeci = tekst.Split(' ');
+            for (int i = 0; i < rijeci.Length; i++)
+            { 
+                Console.WriteLine(rijeci[i]);
+            }
+
+            Console.WriteLine("");
+
+            foreach (string rijec in rijeci)
+            {
+                Console.WriteLine(rijec);
+            }
+
+            Console.WriteLine("\n -- Umetanje --");
+            Console.WriteLine(tekst.IndexOf("radimo"));
+
+            Console.WriteLine(tekst.Insert(tekst.IndexOf("radimo"), "intenzivno"));
+
+
+            Console.ReadKey();
+        }
+    }
+}
